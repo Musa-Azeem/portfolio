f24fee6e529a31c466950a8f22ae2642ea032f5a
Musa Azeem <musa.mazeem@gmail.com>
2023-01-30 23:21:20 -0500

    working

--- backend/my-app/app.js
+++ backend/my-app/app.js
@@ -16,20 +16,23 @@ app.use((req, res, next) => {   // request, response, next middleware function
 // use routes relative to /api/projects/
 app.use('/api/projects/', projectsRoutes)
 
-app.get('/', (req, res) => {    // request, response
-    res.json({mssg: 'Welcome to the app'})
+app.get('/api', (req, res) => {    // request, response
+    res.json({mssg: 'Welcome to the Api'})
 })
 
 // Connect to MongoDB on Atlas
-mongoose.set('strictQuery', false);
-mongoose.connect(process.env.MONGO_URI)
-  // call when connection is complete - only listen for request once connection to DB is established
-  .then(() => {
-    // Listen for Requests in port defines in .env
-    app.listen(process.env.PORT), () => {
-      console.log('Connected to DB and listening on port', process.send.MONGO_URI)
-    }
-  })
-  .catch((error) => {     // Catch error
-    console.log(error)
-  })
+// mongoose.set('strictQuery', false);
+// mongoose.connect(process.env.MONGO_URI)
+//   // call when connection is complete - only listen for request once connection to DB is established
+//   .then(() => {
+//     // If not on aws:   Listen for Requests in port defines in .env
+//     // app.listen(process.env.PORT), () => {
+//     //   console.log('Connected to DB and listening on port', process.send.MONGO_URI)
+//     // }
+//     console.log('Connected to DB')
+//   })
+//   .catch((error) => {     // Catch error
+//     console.log(error)
+//   })
+
+module.exports = app
