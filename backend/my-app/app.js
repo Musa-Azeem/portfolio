5e8bec0bf30aca28b2270cfe863555ce9b15203f
MMAzeem3 <musa.mazeem@gmail.com>
2023-01-31 10:01:45 -0500

    local

--- backend/my-app/app.js
+++ backend/my-app/app.js
@@ -3,6 +3,12 @@ const express = require('express')
 const mongoose = require('mongoose')
 const projectsRoutes = require('./routes/projectRoutes')
 
+// Set deployment mode to AWS ('true') or local ('false') - default AWS
+var AWS = true
+if (process.env.AWS) {
+  AWS = !(process.env.AWS == 'false')
+}
+
 const app = express()
 
 // Middleware
@@ -25,11 +31,15 @@ mongoose.set('strictQuery', false);
 mongoose.connect(process.env.MONGO_URI)
   // call when connection is complete - only listen for request once connection to DB is established
   .then(() => {
-    // If not on aws:   Listen for Requests in port defines in .env
-    // app.listen(process.env.PORT), () => {
-    //   console.log('Connected to DB and listening on port', process.send.MONGO_URI)
-    // }
-    console.log('Connected to DB')
+    // If not on AWS, listen for requests in port defined in .env
+    if (!AWS) {
+      app.listen(process.env.PORT), () => {
+        console.log('Connected to DB and listening on port', process.send.MONGO_URI)
+      }
+    }
+    else {
+      console.log('Connected to DB')
+    }
   })
   .catch((error) => {     // Catch error
     console.log(error)
