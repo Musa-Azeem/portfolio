d09ac91540727ea3233a8de5900ce32ce33a45e1
MMAzeem3 <musa.mazeem@gmail.com>
2023-02-02 15:43:41 -0500

    drive link

--- frontend/src/components/ProjectCard.js
+++ frontend/src/components/ProjectCard.js
@@ -1,7 +1,9 @@
 import React from "react"
-import cluster from '../assets/images/cluster.png'
+// import cluster from '../assets/images/cluster.png'
 
 const ProjectCard = ({ project }) => {
+  const cluster = "https://drive.google.com/uc?export=view&id=142UDLhTMqBkqIbAko2UsowqyvEyf6qJl"
+  // https://drive.google.com/uc?export=view&id=[image id]
   return (
     <section className="projectCard">
       <img src={ cluster } alt={ "Image for " + project.title }></img>
