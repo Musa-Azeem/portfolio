c9b65339fae21fa2d39fe660e5efc4e20924ec8d
MMAzeem3 <musa.mazeem@gmail.com>
2023-03-28 16:02:58 -0400

    par

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -2,8 +2,13 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className="header">
-      <h1>About</h1>
+    <div className="aboutPage">
+      <div className="header">
+        <h1>About</h1>
+      </div>
+      <p>
+        I am a junior at the University of South Carolina studying computer engineering. I have a strong interest in technology and how it can be used to solve real-world problems, especially in fields of medicine. As an undergraduate research assistant in the Valafar Group, I am working with a team on Android Wear OS development and the applications of machine learning in human activity recognition. After graduation, I plan to complete an accelerated master’s degree in computer science before transitioning to the data science or application development industry.
+      </p>
     </div>
   )
 }
